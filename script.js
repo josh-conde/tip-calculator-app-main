d47d94b1803faa0acf1a22aeb3b131929ab1eba7
josh-conde <joshconde@aol.com>
2024-05-22 10:41:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const fivePercent = getElementById('5');
+const tenPercent = getElementById('10');
+const fifteenPercent = getElementById('15');
+const twentyFivePercent = getElementById('25');
+const fiftyPercent = getElementById('50');
+const tipAmount = getElementById('tip-amount');
+const total = getElementById('total');
+
+
+
